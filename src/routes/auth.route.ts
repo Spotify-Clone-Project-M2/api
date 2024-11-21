4a2336890134836d4ebaf03dd37fb9fc99d6db75
KevOneRedOne <kevinalves@hotmail.fr>
2024-11-21 16:51:55 +0100

    feat(init): setup linter, prettier and husky configuration

--- src/routes/auth.route.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from "express";
-const router = express.Router();
-
-/* GET users listing. */
-router.get("/", function (req: Request, res: Response) {
-  res.send("respond with a resource");
-});
-
-module.exports = router;
--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const authRoute = require("./auth.route");
-
-router.use("/auth", authRoute);
-
-module.exports = router;
