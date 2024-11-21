088840fc944757b8f24605013d87de672f45834d
KGROLLEMUND <kyllian.grollemund@ynov.com>
2024-11-21 19:51:56 +0100

    feat: add package pino pino-pretty @types/pino

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { PrismaClient } from '@prisma/client';
-<<<<<<< HEAD
 import createError from 'http-errors';
 import path from 'path';
 import dotenv from 'dotenv';
@@ -10,27 +9,9 @@ import indexRouter from './routes/index';
 import sessionMiddleware from './middlewares/session';
 import fileCacheMiddleware from './middlewares/fileCache';
 import cacheMiddleware from './middlewares/cache';
-=======
-const createError = require('http-errors');
-const path = require('path');
 import logger from './logger';
-
-const indexRouter = require('./routes/index');
-const dotenv = require('dotenv');
-const swaggerUIPath = require('swagger-ui-express');
-const swaggerjsonFilePath = require('../docs/swagger.json');
-
-const router = require('./routes/index.ts');
->>>>>>> db28730 (feat: update index.ts)
 
 dotenv.config();
-import express, { Request, Response } from 'express';
-const createError = require('http-errors');
-const path = require('path');
-import logger from './logger';
-const dotenv = require('dotenv');
-
-const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -79,22 +60,4 @@ app.use(
     swaggerUIPath.setup(swaggerjsonFilePath),
 );
 
-<<<<<<< HEAD
 export default app;
-=======
-app.listen(PORT, () => {
-    logger.info('Server is running on address: http://localhost:' + PORT);
-    logger.info(
-        'API documentation is running on address: http://localhost:' +
-            PORT +
-            '/api-docs',
-    );
-}).on('error', (error: any) => {
-    // gracefully handle error
-    logger.error(error);
-});
-
-export {};
-
-module.exports = app;
->>>>>>> db28730 (feat: update index.ts)
