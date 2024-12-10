f129f4b4eca526e320158c0e8295c0232f76fb24
KGROLLEMUND <kyllian.grollemund@ynov.com>
2024-12-10 15:53:21 +0100

    feat: added CRUD user + route + test unitaire

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoute from './auth.route';
 import artistRoute from './artist.route';
 import albumRoute from './album.route';
+import userRoute from './user.route';
 
 const router = express.Router();
 
@@ -41,5 +42,18 @@ router.use(
  *         description: Album resource
  */
 router.use('/album', albumRoute);
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     tags:
+ *       - User
+ *     summary: Get user resource
+ *     responses:
+ *       200:
+ *         description: User resource
+ */
+router.use('/users', userRoute);
 
 export default router;
