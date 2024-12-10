1f9e4e5f63c2cae0cff02f3233fee2dcc1db40cc
Tadayoshi123 <yankill@outlook.fr>
2024-12-10 12:16:41 +0100

    feat: added basic CRUD to artiste

---
+++ src/routes/artist.route.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+    createArtist,
+    getArtists,
+    getArtistById,
+    updateArtist,
+    deleteArtist,
+} from '../controllers/artist.controller';
+
+const router = express.Router();
+
+router.post('/', createArtist);
+router.get('/', getArtists);
+router.get('/:id', getArtistById);
+router.put('/:id', updateArtist);
+router.delete('/:id', deleteArtist);
+
+export default router;
